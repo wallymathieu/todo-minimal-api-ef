8b3a9ba12b30f62b9f0c1061bbc42812c9b776cd
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-06-23 11:29:14 +0300

    Use IAsyncLifetime

--- tests/ApiFixture.cs
+++ tests/ApiFixture.cs
@@ -13,63 +13,73 @@ using Newtonsoft.Json.Linq;
 using Testcontainers.MsSql;
 using Xunit;
 namespace Todo.Api.Tests;
-public class DbFixture:IDisposable
+public class DbFixture : IAsyncLifetime
 {
-    // ugly (should await start container):
-    protected Lazy<MsSqlContainer> _container = new(() =>
+    private readonly MsSqlContainer _dbContainer;
+
+    public DbFixture()
     {
-        var _dbContainer = new MsSqlBuilder()
+        _dbContainer = new MsSqlBuilder()
            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
            .WithPassword("Strong_password_123!")
            .WithHostname(Guid.NewGuid().ToString("N"))
            .Build();
-        _dbContainer.StartAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
         var opts = new DbContextOptionsBuilder()
             .UseSqlServer(_dbContainer.GetConnectionString())
             .Options;
         using var db = new TodoDb(opts);
-        db.Database.EnsureCreated();
-        return _dbContainer;
-    });
+        await db.Database.EnsureCreatedAsync();
+    }
 
-    public virtual void Dispose()
+    public async Task DisposeAsync()
     {
-        _container.Value.DisposeAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        await _dbContainer.DisposeAsync();
     }
+
+    public string GetConnectionString() => _dbContainer.GetConnectionString();
 }
-public class ApiFixture: DbFixture,IDisposable
+public class ApiFixture : IAsyncLifetime
 {
-    WebApplicationFactory<Program> Create()
-    {
-        var factory = new WebApplicationFactory<Program>();
-        return factory.WithWebHostBuilder(c => c.UseConfiguration(new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string?> {
-                   {"AZURE_SQL_CONNECTION_STRING_KEY", "CONNECTION_STRING"},
-                   {"CONNECTION_STRING",_container.Value.GetConnectionString()}
-                }).Build()));
-    }
     private readonly WebApplicationFactory<Program> _testServer;
+    private readonly DbFixture _dbFixture;
+    private readonly WebApplicationFactory<Program> factory;
+
     public ApiFixture()
     {
-        _testServer = Create();
+        _dbFixture = new DbFixture();
+        factory = new WebApplicationFactory<Program>();
+        _testServer = factory.WithWebHostBuilder(c => c.UseConfiguration(new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string?> {
+                   {"AZURE_SQL_CONNECTION_STRING_KEY", "CONNECTION_STRING"},
+                   {"CONNECTION_STRING", _dbFixture.GetConnectionString()}
+                }).Build()));
     }
 
-    public override void Dispose()
+    public async Task InitializeAsync()
+    {
+        await _dbFixture.InitializeAsync();
+    }
+
+    public async Task DisposeAsync()
     {
         _testServer.Dispose();
-        base.Dispose();
+        await _dbFixture.DisposeAsync();
     }
+
     public TestServer Server => _testServer.Server;
 }
 
-public class ContractTests : IClassFixture<ApiFixture>
+public class ContractTests(ApiFixture fixture) : IClassFixture<ApiFixture>
 {
-    private readonly ApiFixture _fixture;
-    public ContractTests(ApiFixture fixture) => this._fixture = fixture;
 
     [Fact]
     public async Task Can_save_and_get_todo()
     {
-        using var client = _fixture.Server.CreateClient();
+        using var client = fixture.Server.CreateClient();
         var createdTodo = await client.PostAsync("/lists",
             new StringContent(@"{
                         ""Name"": ""Test""
